2a249442c09674c7fbf1e531f983917b1418eb0c
dekel <dekel@vmware.com>
2023-04-26 14:34:13 -0700

    WIP

--- main.go
+++ main.go
@@ -164,7 +164,7 @@ func parseHost (r *http.Request) (teamName string, apiDomain string)  {
     host := r.Host
     hostParts := strings.Split(host, ".")
 	teamName = strings.ToUpper(hostParts[1])
-	apiDomain = hostParts[1] + "." hostParts[2] + "." + hostParts[3]
+	apiDomain = hostParts[1] + "." + hostParts[2] + "." + hostParts[3]
 
 	return
  
